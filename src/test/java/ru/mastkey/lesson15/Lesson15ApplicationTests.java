537e302ee76308b43e1eae3a40c05d434d2f6896
mastkey <ilya.fetyukhin@mail.ru>
2024-11-19 19:52:19 +0300

    init

---
+++ src/test/java/ru/mastkey/lesson15/Lesson15ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mastkey.lesson15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson15ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
